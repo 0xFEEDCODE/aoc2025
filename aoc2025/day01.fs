f32c6908ba37b2e2a3b889dba3682baea5f9485c
0xFEEDCODE <samo.seman96@gmail.com>
2025-12-01 10:56:10 +0100

    x

--- aoc2025/day01.fs
+++ aoc2025/day01.fs
@@ -5,30 +5,28 @@ open aoc2025.util
 [<Literal>]
 let RotationSlots = 100
 
-let performOperation (op: int -> int -> int, dist) prevDial = op prevDial (dist % RotationSlots)
+let performRotation (op: int -> int -> int, dist) prevDial = op prevDial (dist % RotationSlots)
 
 let getNewClicks countRotations opAndDistance prevDial =
     let rotationClicks = (snd opAndDistance) / RotationSlots
 
     countRotations?(rotationClicks, 0)
-    + match (performOperation opAndDistance prevDial) with
-      | nd when countRotations && nd < 0 && prevDial <> 0 -> 1
-      | nd when countRotations && nd > RotationSlots && prevDial <> 0 -> 1
+    + match (performRotation opAndDistance prevDial) with
+      | res when countRotations && res < 0 && prevDial <> 0 -> 1
+      | res when countRotations && res > RotationSlots && prevDial <> 0 -> 1
       | 0
       | RotationSlots -> 1
       | _ -> 0
 
 let getNewDial opAndDistance prevDial =
-    match (performOperation opAndDistance prevDial) with
-    | nd when nd < 0 -> RotationSlots - abs nd
-    | nd when nd >= RotationSlots -> nd % RotationSlots
-    | nd -> nd
+    match (performRotation opAndDistance prevDial) with
+    | res when res < 0 -> RotationSlots - abs res
+    | res when res >= RotationSlots -> res % RotationSlots
+    | res -> res
 
 let rotateLock countRotations (accClicks, prevDial) opAndDistance =
     let newDial = getNewDial opAndDistance prevDial
-
     let newClicks = getNewClicks countRotations opAndDistance prevDial
-
     (accClicks + newClicks, newDial)
 
 let solve () =
@@ -46,9 +44,10 @@ let solve () =
             let dist = x[1..] |> int
             (rotOp, dist))
 
-    let dial = 50
-    let ans1, _ = ((0, dial), doc) ||> Seq.fold (rotateLock false)
-    let ans2, _ = ((0, dial), doc) ||> Seq.fold (rotateLock true)
+    let initialDial = 50
+
+    let ans1, _ = ((0, initialDial), doc) ||> Seq.fold (rotateLock false)
+    let ans2, _ = ((0, initialDial), doc) ||> Seq.fold (rotateLock true)
 
     printfn $"%A{ans1}"
     printfn $"%A{ans2}"
