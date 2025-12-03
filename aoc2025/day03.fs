eb6024df0e750b504d49815a7e64c82ac7714971
some00000001 <samo.seman96@gmail.com>
2025-12-03 11:09:08 +0100

    c

--- aoc2025/day03.fs
+++ aoc2025/day03.fs
@@ -4,67 +4,32 @@ open System
 open Microsoft.FSharp.Core
 open aoc2025.util
 
-let useSampleInput = 0
+let useSampleInput = 1
 
-
-let asDoubleDigit (x : int * int) = ((fst x).ToString() + (snd x).ToString()) |> int
-
-let asNum (x : string) =
-    if x.Length = 0 then
-        float 0
-    else
-        UInt64.Parse(x) |> float
-
-let alg (n: 'a list) =
-    
-    let l = n |> Seq.length
-    
-    let mutable bestFound = (0,0)
-    
-    for i in 0..l-1 do
-        for j in (i+1)..l-1 do
-            let temp = (n[i], n[j])
-            
-            if (asDoubleDigit temp) >  (asDoubleDigit bestFound) then
-                bestFound <- temp
-        
-            
-    asDoubleDigit bestFound
-    
-
-let alg2 (n: int list) =
-    let l = n |> Seq.length
-    
-    let mutable nLeft = 12
+let alg nDigitsTarget (n: int list) =
+    let nElements = n |> Seq.length
     
     let indexed = n |> smapi |> Seq.toList
     
-    let mutable res = []
-    let mutable i = 0
-    
-    while nLeft > 0 do
-        
-        let c = n[i]
-        
-        let betterDigits = indexed[(i+1)..] |> Seq.where(fun x -> (snd x) > c)
-        let nBetterDigits = betterDigits |> Seq.length
-        let nextBetterDigit = betterDigits |> Seq.tryHead
-        
-        let cond =
-            if nextBetterDigit.IsNone then
-                false
-            else
-                let (j,v) = nextBetterDigit.Value
-                not (j+nLeft <= l)
+    let rec loop i acc nLeft =
+        if nLeft = 0 then
+            acc
+        else
+            let valueAtCurrentIndex = n[i]
             
-        if nBetterDigits = 0 || cond then
-            res <- res @ [c]
-            nLeft <- nLeft-1
-        
-        &i += 1
-        
-    res |> Seq.fold(fun acc x -> acc + x.ToString()) "" |> uint64
-        
+            let betterDigits = indexed[(i+1)..] |> Seq.where(fun (_, digit) -> digit > valueAtCurrentIndex)
+            let nextBetterDigit = betterDigits |> Seq.tryHead
+            
+            match nextBetterDigit with
+            | Some(betterDigitIndex, _) when (betterDigitIndex+nLeft-1 < nElements) ->
+                // Value at current index is not best candidate, move to the best candidate index instead
+                loop betterDigitIndex acc nLeft
+            | _ ->
+                // Value at current index is best candidate, add to acc and increment idx
+                loop (i+1) (acc @ [valueAtCurrentIndex]) (nLeft-1)
+            
+    
+    (loop 0 [] nDigitsTarget) |> Seq.fold(fun acc x -> acc + x.ToString()) "" |> uint64
     
 let solve () =
     let io = aocIO
@@ -72,8 +37,8 @@ let solve () =
     
     let banks = (inp |> Seq.map(fun x -> x |> Seq.map(fun y -> Int32.Parse(y.ToString())) |> Seq.toList) |> Seq.toList)
     
-    let ans1 = banks |> Seq.map alg |> Seq.sum
-    let ans2 = banks |> Seq.map (fun x -> uint64 (alg2 (x))) |> Seq.sum 
+    let ans1 = banks |> Seq.map (alg 2) |> Seq.sum
+    let ans2 = banks |> Seq.map (alg 12) |> Seq.sum 
     
     printfn $"%A{ans1}"
     printfn $"%A{ans2}"
